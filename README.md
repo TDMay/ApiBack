15568ec585289ca35bec0b503fa5276ace606011
Maykon Bogéa <114835127+TDMay@users.noreply.github.com>
2024-06-23 16:51:34 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+ # 🎮 Scientific Odyssey | by XP Studio
+
+<em>"Prepare-se para começar uma jornada com a Dra. Carla pela Universidade Armageddon e salvá-la de experimentos altamente perigosos."</em>
+
+#### 🖥️: https://yasmimlunna.github.io/scientific-odyssey/
+![Captura de tela 2024-06-20 232316](https://github.com/YasmimLunna/scientific-odyssey/assets/27229715/c4210e35-3d20-4adc-b20f-332946ec39d8)
+___
+
+ # 👥 A XP Studio |
+
+- 👾 Samilla Ferreira Gusmão, Sônia Maria De Oliveira Lima, Hedilane Dos Santos De Araujo, Iza Vitória Carvalho E Carvalho, João José Chagas Neto, Leonardo Luis Costa Santos, Cassiano Fernandes Silva Neto, Raimundo Victor Cardoso Cruz, Laércio Henrique Filgueiras Santos, Luís Gabriel Da Silva Cândido, Yasmim Luna Athaydes Martins, Lucas Leonardo Dos Santos Campos, Ludmilla Silva Vaz, Hellon Pereira Alves, Lianna Cavalcante Ribeiro, Marcelo Henrique Bastos Rodrigues, Karmaykell Chagas Silva, Erika Beatriz Martins Sousa, Maykon Bogéa Martins, Mariana De Sousa Lopes.
+  
+ # 💡 O Projeto |
+
+- 📋 O projeto foi um desafio proposto pela equipe do programa Trilhas Inova Maranhão para que possamos exercer a nossa criatividade e conhecimentos da área da tecnologia.
+  
+- 💼 Somos uma equipe com conhecimento em diversas áreas da tecnologia, envolvendo ciência de dados, ux/ui, front-end e back end e jogos.
+  
+- 🛠️ Usamos o Miro, Figma, Power BI, GameMaker, VS Code, e muito brainstorm para produzir todo o projeto.
---
+++ js/script.js
@@ -0,0 +1,156 @@
+// Importando as funções de validação de senha
+import { validaSenha } from "./valida-senha.js";
+import { validaConfirmaSenha } from "./valida-confirmaSenha.js";
+
+// Selecionando os elementos do DOM
+const camposDoFormulario = document.querySelectorAll("[required]");
+const formulario = document.querySelector("[data-formulario]");
+const buttonMenu = document.querySelector('.button__menu-mobile');
+const menuMobile = document.querySelector('.cabecalho__mobile');
+let menuOpen = false;
+
+// URL do servidor Express
+const URL_SERVIDOR = 'http://localhost:3000/usuarios';
+
+// Sistema de Mensagem
+const exibirMensagem = (display,cor,texto) =>{
+    const mensagem = document.querySelector(".formulario__mensagem");
+    mensagem.style.display = display;
+    mensagem.innerHTML = texto;
+    mensagem.style.color = cor;
+}
+
+// Adicionando evento de click ao botão de menu do mobile
+buttonMenu.addEventListener('click', () => {
+    menuOpen ? menuMobile.classList.add('invisible') : menuMobile.classList.remove('invisible');
+    menuOpen ? buttonMenu.children[0].setAttribute('class', 'fa-solid fa-bars') : buttonMenu.children[0].setAttribute('class', 'fa-solid fa-xmark');
+    menuOpen = !menuOpen;
+});
+
+// Evento de submit do formulário
+formulario.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const listaRespostas = {
+        "nickname": e.target.elements["nickname"].value,
+        "email": e.target.elements["email"].value,
+        "senha": e.target.elements["senha"].value,
+        "confirmarSenha": e.target.elements["confirmarSenha"].value
+    };
+
+    // Verificar todos os campos ao submeter o formulário
+    let formularioValido = true;
+    camposDoFormulario.forEach((campo) => {
+        verificaCampo(campo);
+        if (!campo.checkValidity()) {
+            formularioValido = false;
+        }
+    });
+
+    // Se todos os campos são válidos, enviar os dados ao servidor
+    if (formularioValido) {
+        try {
+            const response = await fetch(URL_SERVIDOR, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(listaRespostas),
+            });
+
+            if (!response.ok) {
+                const errorResponse = await response.json();
+                throw new Error(errorResponse.error);
+            }
+
+            const newUser = await response.json();
+            console.log('Novo usuário criado:', newUser);
+            exibirMensagem("flex","green","Conta Criada Com Sucesso!");
+
+            // Lógica adicional após criar o usuário (redirecionamento, mensagem de sucesso, etc.)
+
+        } catch (error) {
+            console.error('Erro ao criar usuário:', error);
+            exibirMensagem("flex","red",`${error.message}`);
+            //alert(`Erro ao criar usuário: ${error.message}`);
+            // Tratamento de erro (exibir mensagem para o usuário, por exemplo)
+        }
+    }
+});
+
+// Adicionando eventos de blur e invalid ao campos do formulário
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo));
+    campo.addEventListener("invalid", evento => evento.preventDefault());
+});
+
+// Tipos de erros e mensagens correspondentes
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'tooShort',
+    'customError'
+];
+
+const mensagens = {
+    nome: {
+        valueMissing: "O campo de nome não pode estar vazio.",
+        tooShort: "Por favor, preencha com um nome válido.",
+        patternMismatch: "Por favor, preencha o nome apenas com letras."
+    },
+    email: {
+        valueMissing: "O campo de e-mail não pode estar vazio.",
+        typeMismatch: "Por favor, preencha com um email válido.",
+        tooShort: "Por favor, preencha com um e-mail válido."
+    },
+    senha: {
+        valueMissing: "O campo de Senha não pode estar vazio.",
+        typeMismatch: "Por favor, preencha com uma Senha válida.",
+        tooShort: "A senha deve conter 8 dígitos.",
+        customError: "A senha deve conter letras e números.",
+    },
+    confirmarSenha: {
+        valueMissing: "O campo de Confirmação de Senha não pode estar vazio.",
+        typeMismatch: "Por favor, preencha com uma Senha válida.",
+        tooShort: "A senha deve conter 8 dígitos.",
+        customError: "As senhas não coincidem."
+    },
+    termos: {
+        valueMissing: 'Você deve aceitar nossos termos antes de continuar.',
+    }
+};
+
+// Função que verifica cada campo e trata os respectivos erros
+function verificaCampo(campo) {
+    let mensagem = "";
+    campo.setCustomValidity('');
+
+    // Validando senha
+    if (campo.name === 'senha') {
+        validaSenha(campo);
+    }
+
+    // Validando confirmação de senha
+    if (campo.name === 'confirmarSenha') {
+        const senha = document.querySelector("#senha").value;
+        validaConfirmaSenha(campo, senha);
+    }
+
+    // Associando o erro ao campo específico
+    tiposDeErro.forEach(erro => {
+        if (campo.validity[erro]) {
+            mensagem = mensagens[campo.name][erro];
+            console.log(mensagem);
+        }
+    });
+
+    const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
+    const validadorDeInput = campo.checkValidity();
+
+    if (!validadorDeInput) {
+        mensagemErro.textContent = mensagem;
+    } else {
+        mensagemErro.textContent = "";
+    }
+}
